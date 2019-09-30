f47b048c95742d4abbf19fc63c2bb875cf0c5d7f
Anderson <andersons.code@gmail.com>
2019-09-30 20:12:32 -0300

    feat: koa and http server created

---
+++ api/application.js
@@ -0,0 +1,38 @@
+/**
+ * Application file
+ * This is the application file, where we return an koajs server with middlewares.
+ * 
+ * @module Application
+ */
+
+import * as http from 'http'
+import Koa from 'koa'
+import cors from '@koa/cors'
+import bodyParser from 'koa-bodyparser'
+import compress from 'koa-compress'
+import { logger } from '../infra/logger'
+
+/**
+ * Creates and returns a new Koa application.
+ * @return {Promise<http.Server>} The configured app.
+ */
+
+export async function createServer() {
+  logger.debug('Creating server...', { scope: 'startup' })
+  const app = new Koa()
+
+  app
+    .use(compress())
+    .use(cors())
+    .use(bodyParser())
+
+  const server = http.createServer(app.callback())
+
+  server.on('close', () => {
+    logger.debug('Server closing, bye!', { scope: 'closing' })
+  })
+
+  logger.debug('Server created, ready to listen', { scope: 'startup' })
+
+  return server
+}
