4649eb88888e3672557d446b64f56dcfcd59c2d0
Anderson <andersons.code@gmail.com>
2019-09-30 21:21:41 -0300

    feat: TransactionSchema

---
+++ infra/database/models/transactionModel.js
@@ -0,0 +1,50 @@
+/**
+ * Transaction Schema
+ * This file describes the Transaction Model
+ * 
+ * @module TransactionSchema
+ */
+
+import mongoose from 'mongoose';
+
+const TransactionSchema = new mongoose.Schema({
+  terminal_id: {
+    type: String,
+    required: true,    
+  },
+  nsu: {
+    type: String,
+    required: true,
+  },
+  grossValue: {
+    type: Number,
+    required: true, 
+  },
+  netValue: {
+    type: Number,
+    required: true, 
+  },
+  fee: {
+    type: Number,
+    required: true, 
+  },
+  cardBrand: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+    lowercase: true
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+  },
+  availableData: {
+    type: Date,
+    required: true,
+  }
+});
+
+export const TransactionModel = mongoose.model('Transaction', TransactionSchema);
