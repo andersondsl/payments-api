142a61a88c058c97c962eefb5645a53e37a30f28
Anderson <andersons.code@gmail.com>
2019-09-30 22:09:16 -0300

    feat: transaction repository

---
+++ domain/transaction/transactionRepository.js
@@ -0,0 +1,32 @@
+/**
+ * Transaction repository.
+ * This module handle all requests with the Transaction Collection in mongo databases.
+ * 
+ * @module TransactionRepository
+ */
+
+import mongoose from 'mongoose'
+import { logger } from '../../infra/logger'
+const Transaction = mongoose.model('Transaction')
+
+const handleMongoQuery = async (queryCallback) => {
+  try {
+    let result = await queryCallback;
+    return result 
+  } catch (error) {
+    logger.error(error, { scope: 'Mongo Repository' })
+    return null;
+  }
+}
+
+export const transactionRepository = {
+    getAllTransactions : async () => {
+        return handleMongoQuery(Transaction.find({}))
+    },
+    getTransactionById : async (id) => {
+        return handleMongoQuery(Transaction.findById(id))
+    },
+    createTransaction: async (data) => {
+        return handleMongoQuery(Transaction.create(data))
+    }
+};
