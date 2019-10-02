eab416d6a6472d9c0ada8da9fd4141d6aff4e0b2
Anderson <andersons.code@gmail.com>
2019-10-01 22:49:19 -0300

    feat: validateing access

--- api/controllers/transacionController.js
+++ api/controllers/transacionController.js
@@ -9,6 +9,15 @@
 
 import { transactionService } from "../../domain/transaction/transactionService";
 
+const validateAccess = (ctx, user, password) => {
+  if (ctx.state.user !== user || ctx.state.password !== password) {
+    ctx.status = 404;
+    ctx.body = "Unauthorized access";
+    return false;
+  }
+  return true;
+};
+
 const handleResponse = async (ctx, callback) => {
   let { status, data } = await callback;
   ctx.status = status;
@@ -16,25 +25,50 @@ const handleResponse = async (ctx, callback) => {
 };
 
 export const index = async (ctx, next) => {
+  if (!validateAccess(ctx, "portal", "123456")) {
+    return;
+  }
+
   await handleResponse(ctx, transactionService.getAllTransactions());
 };
 
 export const findById = async (ctx, next) => {
+  if (!validateAccess(ctx, "terminal", "123456")) {
+    return;
+  }
+
   const { id } = ctx.params;
   await handleResponse(ctx, transactionService.getTransactionById(id));
 };
 
 export const create = async (ctx, next) => {
+  if (!validateAccess(ctx, "terminal", "123456")) {
+    return;
+  }
+
   const { nsu, valor, bandeira, modalidade, horario } = ctx.request.body;
 
-  await handleResponse(
-    ctx,
-    transactionService.createTransaction({
-      nsu,
-      value: valor,
-      cardBrand: bandeira,
-      type: modalidade,
-      createdAt: horario
-    })
-  );
+  let result = await transactionService.createTransaction({
+    nsu,
+    value: valor,
+    cardBrand: bandeira,
+    type: modalidade,
+    createdAt: horario
+  });
+
+  if (!result) {
+    ctx.status = 400;
+    ctx.body = "Not Found";
+  }
+
+  ctx.status = 200;
+  ctx.body = {
+    nsu,
+    valor: result.data.grossValue,
+    liquido: result.data.netValue,
+    bandeira: result.data.cardBrand,
+    modalidade: result.data.type,
+    horario: result.data.createdAt,
+    disponivel: result.data.availableDate
+  };
 };
